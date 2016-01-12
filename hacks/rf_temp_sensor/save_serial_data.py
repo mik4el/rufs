7ce7986eeaa90c0f7df25a8f6b5c23ba61c28f25
Mikael Andersson <mik4el.4ndersson@gmail.com>
2016-01-12 22:48:40 +0100

    one more adress to save serial

--- hacks/rf_temp_sensor/save_serial_data.py
+++ hacks/rf_temp_sensor/save_serial_data.py
@@ -3,7 +3,8 @@ from time import strftime
 from datetime import datetime, time
 
 #ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)  # linux address
-ser = serial.Serial('/dev/cu.wchusbserialfa140', 9600, timeout=1)  # osx address 
+#ser = serial.Serial('/dev/cu.wchusbserialfa140', 9600, timeout=1)  # osx address 1 
+ser = serial.Serial('/dev/cu.wchusbserialfd130', 9600, timeout=1)  # osx address 2
 
 try:
 	while 1:
